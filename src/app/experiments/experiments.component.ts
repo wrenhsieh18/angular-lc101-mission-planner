3d079d80378faf84d7b321f4658cf797fdd1c3d1
Wren Hsieh <wrenhsieh18@gmail.com>
2021-12-03 14:27:12 -0600

    finshed bonus

--- src/app/experiments/experiments.component.ts
+++ src/app/experiments/experiments.component.ts
@@ -8,8 +8,31 @@ import { Component, OnInit } from '@angular/core';
 export class ExperimentsComponent implements OnInit {
    experiments: string[] = ['Moon soil sample', 'Plant growth in orbit', 'Human bone density changes', 'Water droplet podcast for grades K-5', 'Satellite launch'];
 
+   plannedExperiments: string[] = [];
+   maxExperiments: number = 5;
+
    constructor() { }
 
    ngOnInit() { }
+
+   canAdd(experiment: string, experimentList: string[]): boolean {
+    if (!experimentList.includes(experiment) && this.plannedExperiments.length < this.maxExperiments) {
+      return true;
+    } else {
+      return false;
+    }
+   }
+
+   addToPlan(experiment: string): void {
+     if (this.canAdd(experiment, this.plannedExperiments) && this.plannedExperiments.length < this.maxExperiments) {
+      this.plannedExperiments.push(experiment);
+     }
+   }
+
+   addNewExperiment(newExperiment: string): void {
+    if (this.canAdd(newExperiment, this.experiments)) {
+      this.experiments.push(newExperiment);
+    }
+   }
 
 }
